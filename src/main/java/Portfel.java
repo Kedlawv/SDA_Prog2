021394c4d5b6436cce2940ac85b55fea2cea77da
Kedlawv <44945961+Kedlawv@users.noreply.github.com>
2019-07-13 18:35:47 +0200

    Add files via upload

---
+++ src/main/java/Portfel.java
@@ -0,0 +1,15 @@
+public class Portfel {
+   private Pieniadze pln;
+
+    public Portfel(Pieniadze pln) {
+        this.pln = pln;
+    }
+
+    public void add(Pieniadze pieniadze){
+        this.pln = pln.add(pieniadze);
+    }
+
+    public Pieniadze getPln() {
+        return pln;
+    }
+}
